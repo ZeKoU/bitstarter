6f72a5a17a5592eaaf0132a371aa30482b8e848b
Amer Zec <amerzec@gmail.com>
2013-07-08 21:14:57 +0000

    Reading from buffer - index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+  var indexBuf = fs.readFileSync('index.html'); //Should return buffer...
+  response.send(indexBuf.toString());
 });
 
 var port = process.env.PORT || 5000;
